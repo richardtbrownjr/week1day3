8659934a65e7aeaa5a9bb21049a22a1569e67938
Richard Brown <richardtbrownjr@gmail.com>
2016-07-18 22:14:35 -0400

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,32 +1,33 @@
-import re
 phrase = []
 
 def recursion(phrase):
-    #print('String here', phrase
-        if phrase[0] == phrase[-1]:
-            print(phrase)
-            del phrase[0]
-            del phrase[-1]
-            print(phrase)
-            value=len(phrase)
-            if value > 1:
-                recursion(phrase)
-            elif value == 0:
-                print("Palindrome!")
-        else:
-            print("Not a palindrome")
-            return
+    print('String here', phrase)
+    if phrase[0] == phrase[-1]:
+        del phrase[0]
+        del phrase[-1]
+        print(phrase)
+        return
+                #print('   ')
+    else:
+                #return False
+        print("Not a palindrome")
 
 def is_palindrome(phrase):
     # TODO: return True or False if the sentence is or isn't a palindrome
         #print ('is pal',phrase)
         string_of_phrase = ''.join(phrase)
-        #string additions not working yet
-        #phrase = re.sub('[^A-Za-z0-9]','',phrase)
         phrase = list(string_of_phrase)
         #print(phrase[0])
         #print(phrase[-1])
+        count = len(phrase)
+        #print(' is pal', count)
+        length_of_word(count)
+
+def length_of_word(count):
+    #print(count)
+    if count > 1:
         recursion(phrase)
+        count-=1
 
 def main():
     # TODO: put your input/output code here
@@ -35,7 +36,6 @@ def main():
     phrase =[sentence]
 
     is_palindrome(phrase)
-
 
 if __name__ == '__main__':
     main()
